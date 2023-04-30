ccc939532b6626bed92a8b66caeb0544b6c2a87f
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-04-30 22:45:05 +0530

    Replace relative import with absolute import

--- lpython_emulation_pkg/lpython.py
+++ lpython_emulation_pkg/lpython.py
@@ -3,7 +3,7 @@ import os
 import ctypes
 import platform
 from dataclasses import dataclass as py_dataclass, is_dataclass as py_is_dataclass
-from .goto import with_goto
+from goto import with_goto
 
 # TODO: this does not seem to restrict other imports
 __slots__ = ["i8", "i16", "i32", "i64", "f32", "f64", "c32", "c64", "CPtr",
