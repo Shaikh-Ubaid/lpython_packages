a3296457c616f3110dd01bb4b7a9765a2a7fc639
Shaikh Ubaid <shaikhubaid769@gmail.com>
2023-06-15 05:06:08 +0530

    Add latest lpython.py
    Use version similar to lpython

--- lpython_emulation_pkg/setup.py
+++ lpython_emulation_pkg/setup.py
@@ -7,7 +7,7 @@ import setuptools
 # file and read in here.
 REQUIRED_PACKAGES=[]
 
-VERSION="0.0.1.9"
+VERSION="0.0.16.0"
 
 # Read in the project description. We define this in the README file.
 with open("README.md", "r") as fh:
